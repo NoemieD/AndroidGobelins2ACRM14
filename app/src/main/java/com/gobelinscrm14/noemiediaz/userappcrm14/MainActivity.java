b7d5b134413f0d683df2a5fc42de0800850c9915
noemiediaz <noemiediaz4@gmail.com>
2015-10-13 08:59:29 +0200

    Toolbar avec menu

---
+++ app/src/main/java/com/gobelinscrm14/noemiediaz/userappcrm14/MainActivity.java
@@ -0,0 +1,29 @@
+package com.gobelinscrm14.noemiediaz.userappcrm14;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+import com.gobelinscrm14.noemiediaz.userappcrm14.user.LoginFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar mtoolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+}
