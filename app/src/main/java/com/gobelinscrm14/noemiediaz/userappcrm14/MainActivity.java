a627348a453b8f58de560e6f11f3b9a64b8b37e3
noemiediaz <noemiediaz4@gmail.com>
2015-10-13 09:19:00 +0200

    Create Register Fragment

--- app/src/main/java/com/gobelinscrm14/noemiediaz/userappcrm14/MainActivity.java
+++ app/src/main/java/com/gobelinscrm14/noemiediaz/userappcrm14/MainActivity.java
@@ -4,9 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.gobelinscrm14.noemiediaz.userappcrm14.user.LoginFragment;
+import com.gobelinscrm14.noemiediaz.userappcrm14.user.RegisterFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,9 +28,12 @@ public class MainActivity extends AppCompatActivity {
                 .commit();
     }
 
+    //CREATE MENU
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    
 }
